b5a2759c965b7c9719f2b2d7e6e3884c1dd4d0e9
risandi-de-silva <risandidesilva623@gmail.com>
2026-08-19 04:35:41 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,11 +2,12 @@
 // Database Configuration
 $host     = "127.0.0.1";
 $db_name  = "glowcare_db";
+$port = "3308";
 $username = "root";
 $password = "";
 
 // Database Connection via MySQLi
-$conn = new mysqli($host, $username, $password, $db_name);
+$conn = new mysqli($host, $username, $password, $db_name, $port);
 
 if ($conn->connect_error) {
     die("Database Connection Failed: " . $conn->connect_error);
